99a2100894f725b66658cb6bb64276db7e2b4056
beka <juniorbeka63k@gmail.com>
2024-12-03 12:00:23 +0100

    Add simple weather fetcher

---
+++ script.js
@@ -0,0 +1,57 @@
+const messages = [
+  "Hi...",
+  "Welcome to my GitHub page",
+  "I am a Junior Java Web Developer",
+  "Today's weather...",
+];
+
+const message = document.getElementById("message");
+
+let messageIndex = 0;
+
+function updateMessage() {
+  if (messageIndex < messages.length) {
+    message.textContent = messages[messageIndex];
+    messageIndex++;
+  } //else if (messageIndex === messages.length) {
+   // messageIndex = 0;
+  //} 
+  else {
+    clearInterval(messageInterval);
+  }
+}
+
+const messageInterval = setInterval(updateMessage, 2000);
+
+
+async function findGeo() {
+  try {
+    const response = await fetch("http://ip-api.com/json/?fields=61439");
+    const result = await response.json();
+    //console.log(result);
+
+    findWeather(result.lat, result.lon, result.city, result.country);
+  } catch (error) {
+    console.log("Error");
+  }
+}
+
+async function findWeather(latitude, longitude, city, country) {
+  try {
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`);
+    const result = await response.json();
+
+    //console.log(result);
+    const temperature = result.current_weather.temperature;
+    
+    messages[3] = `Weather in ${city}, ${country}: ${temperature}°C`;
+  } catch (error) {
+    messages[3] = "Nice weather u have there...";
+  }
+}
+
+
+
+
+updateMessage();
+findGeo();
